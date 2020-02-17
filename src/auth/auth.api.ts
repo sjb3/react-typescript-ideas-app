4c670c22aafd1ac845e2ed37c67b2c231d2c0d0f
Justin Byun <klofseattle@hotmail.com>
2020-02-16 21:44:46 -0800

    To fix base url issue: contact the author

---
+++ src/auth/auth.api.ts
@@ -0,0 +1,22 @@
+import Axios, { AxiosRequestConfig } from "axios";
+
+export interface ICredentials {
+  username: string;
+  password: string;
+}
+
+export const onLogin = async (data: ICredentials) => {
+  const requestConfig: AxiosRequestConfig = {
+    method: "post",
+    url: process.env.REACT_APP_API_BASE_URL + "/login",
+    data
+  };
+
+  try {
+    const { data: response } = await Axios.request(requestConfig);
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+    return { error: err.response.data.message };
+  }
+};
